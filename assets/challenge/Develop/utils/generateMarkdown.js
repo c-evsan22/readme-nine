0f055ac760e1d8241eac096665129f46a8cc7898
Chris San <christianes322@gmail.com>
2023-10-20 21:44:03 -0500

    changed up the js and got it working

--- assets/challenge/Develop/utils/generateMarkdown.js
+++ assets/challenge/Develop/utils/generateMarkdown.js
@@ -12,9 +12,34 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
 
-`;
-}
+
+  ~
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[contribution](#contribution)
+  *[Credits](#credits)
+  *[License](#license)
+  #Installation
+  ${data.installation}
+  ##Usage
+  ${data.usage}
+  ##Contribution
+  ${data.contribution}
+  ##instructions
+  ${data.instructions}
+  ##Credits
+  ${data.credit}
+  ##License
+  ${data.license}
+
+  #Contact
+  *Github :${data.git}
+  *Linkdin :${data.linkdin}
+  *E-mail: ${data.email} `;
+
+};
+
 
 module.exports = generateMarkdown;
